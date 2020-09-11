6ce93cb44f04a34f878b5c42bb3a1ff8eca97559
Clara Mary Price <claraprice@Claras-MacBook-Pro-6.local>
2020-09-11 11:46:43 +0200

    Funktion vis kaffe

--- kaffe.js
+++ kaffe.js
@@ -1,4 +1,4 @@
-let kaffe;
+let kaffer;
 let filter = "bønne";
 document.addEventListener("DOMContentLoaded", loadJSON)
 
@@ -6,10 +6,21 @@ async function loadJSON() {
     const JSONData = await
     fetch("https://spreadsheets.google.com/feeds/list/1cusyHmJ0ML-csQjEMGf9hkosvQJtib6zNYnf44xYeA4/od6/public/values?alt=json");
     kaffe = await JSONData.json();
-    visKaffe();
+    visKaffer();
 }
 
-function visKaffe() {
+function visKaffer() {
     const templatePointer = document.querySelector("template");
     const listpointer = document.querySelector("#list");
+    kaffer.feed.entry.forEach(kaffe => {
+        if (filter == kaffe.gsx$bønne.$t) {
+            console.log(kaffe);
+            const minKlon = templatePointer.cloneNode(true).content;
+            minKlon.querySelector("h3").textContent = kaffe.gsx$bønne.$t;
+            minKlon.querySelector("img").scr = `images_coffee/${kaffe.gsx$billede.$t}.jpg`;
+            minKlon.querySelector("p").textContent = kaffe.gsx$beskrivelse.$t;
+            listpointer.appendChild(minKlon);
+        }
+    })
+
 }
